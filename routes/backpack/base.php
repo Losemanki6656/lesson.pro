64fdc1188d442da3b8ee9ba57aa432834d011a80
Losemanki6656 <bbiznestg@gmail.com>
2022-12-21 00:50:13 +0500

    asd

--- routes/backpack/base.php
+++ routes/backpack/base.php
@@ -30,8 +30,9 @@ function () {
         Route::post('logout', 'Auth\LoginController@logout');
 
         // Registration Routes...
-        Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('backpack.auth.register');
-        Route::post('register', 'Auth\RegisterController@register');
+        Route::get('register', 'Auth\LoginController@showLoginForm')->name('backpack.auth.register');
+        // Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('backpack.auth.register');
+        // Route::post('register', 'Auth\RegisterController@register');
 
         // if not otherwise configured, setup the password recovery routes
         if (config('backpack.base.setup_password_recovery_routes', true)) {
