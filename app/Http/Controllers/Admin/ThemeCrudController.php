e83dc22ee5be455fbce7dedf97ad9471945d5629
Losemanki6656 <bbiznestg@gmail.com>
2023-01-25 04:25:37 +0500

    asd

--- app/Http/Controllers/Admin/ThemeCrudController.php
+++ app/Http/Controllers/Admin/ThemeCrudController.php
@@ -24,7 +24,7 @@ class ThemeCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation { store as traitStore; }
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation { destroy as traitDestroy; }
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
     public function setup()
@@ -152,25 +152,6 @@ class ThemeCrudController extends CrudController
 
     public function store()
     {
-        // do something before validation, before save, before everything; for example:
-        // $this->crud->addField(['type' => 'hidden', 'name' => 'author_id']);
-    // $this->crud->removeField('password_confirmation');
-
-    // Note: By default Backpack ONLY saves the inputs that were added on page using Backpack fields.
-    // This is done by stripping the request of all inputs that do NOT match Backpack fields for this
-    // particular operation. This is an added security layer, to protect your database from malicious
-    // users who could theoretically add inputs using DeveloperTools or JavaScript. If you're not properly
-    // using $guarded or $fillable on your model, malicious inputs could get you into trouble.
-
-    // However, if you know you have proper $guarded or $fillable on your model, and you want to manipulate 
-    // the request directly to add or remove request parameters, you can also do that.
-    // We have a config value you can set, either inside your operation in `config/backpack/crud.php` if
-    // you want it to apply to all CRUDs, or inside a particular CrudController:
-        // $this->crud->setOperationSetting('saveAllInputsExcept', ['_token', '_method', 'http_referrer', 'current_tab', 'save_action']);
-    // The above will make Backpack store all inputs EXCEPT for the ones it uses for various features.
-    // So you can manipulate the request and add any request variable you'd like.
-    // $this->crud->getRequest()->request->add(['author_id'=> backpack_user()->id]);
-    // $this->crud->getRequest()->request->remove('password_confirmation');
 
         $response = $this->traitStore();
 
@@ -198,5 +179,12 @@ class ThemeCrudController extends CrudController
     protected function setupUpdateOperation()
     {
         $this->setupCreateOperation();
+    }
+
+    public function destroy($id)
+    {
+        CheckCadry::where('theme_id', $id)->delete();
+        $cadry = Theme::find($id)->delete();
+        return true;
     }
 }
