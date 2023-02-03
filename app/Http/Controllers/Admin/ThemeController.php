60d6eb41c118d0c981750e9af1c93fc5454617f5
Losemanki6656 <bbiznestg@gmail.com>
2023-02-03 22:21:51 +0500

    AsD

--- app/Http/Controllers/Admin/ThemeController.php
+++ app/Http/Controllers/Admin/ThemeController.php
@@ -27,7 +27,7 @@ class ThemeController
 
         foreach($cadries as $item)
         {
-            $cadry = Cadry::find($cadry_id);
+            $cadry = Cadry::find($item->cadry_id);
             $item->department_id = $cadry->department_id;
             $item->position_id = $cadry->position_id;
             $item->user_id = backpack_user()->id;
